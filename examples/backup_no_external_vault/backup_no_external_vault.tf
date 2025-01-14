b17ece99267f96ba70d6af439a9d4181b53c7a3a
Caspersonn <lucakasper8@gmail.com>
2025-01-14 14:16:21 +0100

    Add reusable workflow action

--- examples/backup_no_external_vault/backup_no_external_vault.tf
+++ examples/backup_no_external_vault/backup_no_external_vault.tf
@@ -23,7 +23,7 @@ resource "aws_iam_service_linked_role" "backup_service_linked_role" {
 
 module "kms_backup_vault" {
 
-  source = "git@github.com:wearetechnative/terraform-aws-kms.git?ref=v1.0.0"
+  source = "github.com/wearetechnative/terraform-aws-kms.git?ref=96e823e21f41ddcb29c9d9ed04a7f65e74cbac74"
 
   providers = {
     aws = aws
--- role.tf
+++ role.tf
@@ -1,6 +1,6 @@
 # replaces AWSBackupDefaultServiceRole
 module "iam_role" {
-  source = "git@github.com:wearetechnative/terraform-aws-iam-role.git?ref=v1.0.0"
+  source = "github.com/wearetechnative/terraform-aws-iam-role.git?ref=9229bbd0280807cbc49f194ff6d2741265dc108a"
 
   role_name = "AWSBackup_${var.name}"
   role_path = "/AWSBackup/"
